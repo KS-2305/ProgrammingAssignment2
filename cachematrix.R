573a2ba8cc261cff3522dcafdc5cb98a6f14699e
KS-2305 <31883824+KS-2305@users.noreply.github.com>
2019-06-09 18:36:54 +0530

    R Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
+##My function will take the matrix and invert the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  
+  setMatrix <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  
+  getMatrix <- function() x                              
+  setInverse <- function(inverse) invMatrix <<- inverse  
+  getInverse <- function() invMatrix                     
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  
+  invMatrix <- x$getInverse()
+  if(!is.null(invMatrix)) {                       
+    message("Gets Invertible Matrix")  
+    return(invMatrix)                             #invertible matrix return
+  }
+  
+  
+  MatrixData <- x$getMatrix()                    
+  invMatrix <- solve(MatrixData, ...)             
+  x$setInverse(invMatrix)                         
+  return(invMatrix)                              
+  
 }
+
+
+#TESTING OF THE FUNCTION#
+
+TestMatrix <- matrix(2:6,2,2)
+TestMatrix
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
+
+
+
